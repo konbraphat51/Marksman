bdcf2b180b774d48e1d43fcad16e4588fadc6389
konbraphat51 <konbraphat@gmail.com>
2025-04-25 23:05:14 +0900

    Selectable Mark

--- src/components/Mark.vue
+++ src/components/Mark.vue
@@ -1,10 +1,65 @@
-<template></template>
+<template>
+	<div class="mark-container">
+		<svg xmlns="http://www.w3.org/2000/svg">
+			<circle
+				v-for="(radius, index) in radius"
+				:r="radius + '%'"
+				class="area"
+				cx="50%"
+				cy="50%"
+			/>
+
+			<circle
+				v-for="(radius, index) in radius"
+				:r="radius + '%'"
+				class="border"
+				cx="50%"
+				cy="50%"
+			/>
+		</svg>
+	</div>
+</template>
 
 <script>
 export default {
 	name: "Mark",
-	props: {},
-	data() {},
-	mounted() {},
+	data() {
+		return {
+			radius: [45, 41, 37, 33, 29, 25, 21, 17, 13, 9, 5],
+		}
+	},
+	methods: {},
 }
 </script>
+
+<style scoped>
+.mark-container {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 100%;
+}
+
+svg {
+	width: 100%;
+	height: auto;
+}
+
+.area {
+	fill: lightblue;
+	stroke: none;
+	transition: transform 0.2s;
+}
+
+.area:hover {
+	fill: red;
+	transition: fill 0.2s;
+}
+
+.border {
+	fill: none;
+	stroke: black;
+	stroke-width: 2;
+	pointer-events: none;
+}
+</style>
